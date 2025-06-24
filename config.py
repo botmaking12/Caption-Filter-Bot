eabf4c41ac5da22368b55ddf16662b6ac30c485d
Bot Devloper <telegrambotmaking12@gmail.com>
2025-06-24 11:06:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-MONGO_DB_URI = environ.get("MONGO_DB_URI", "")
-FILE_STORE_CHANNEL = int(environ.get("FILE_STORE_CHANNEL", 0))
+API_ID = int(environ.get("API_ID", "25935455"))
+API_HASH = environ.get("API_HASH", "47cb45d81d6e23bfc68671ea78cc6f9c")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7968665654:AAH_EFmqMrKH16A05sSvb-6yVPaPN-L9cRk")
+MONGO_DB_URI = environ.get("MONGO_DB_URI", "mongodb+srv://tushuu:tushuu@tushuu.uldc...yourlink...")
+FILE_STORE_CHANNEL = int(environ.get("FILE_STORE_CHANNEL", "-1002619448981"))
